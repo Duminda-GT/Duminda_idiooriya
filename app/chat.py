1fe16967b8894a9cb97f409197c9bc0b230908ab
Duminda-greYTech <89688222+Duminda-greYTech@users.noreply.github.com>
2022-05-05 20:08:24 +0530

    chatbot

---
+++ app/chat.py
@@ -0,0 +1,50 @@
+import random
+import json
+import torch
+from model import NeuralNet
+from nltk_ut import bag_of_words, tokenize
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+with open('intents.json', 'r') as f:
+    intents = json.load(f)
+
+FILE = "data.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data["all_words"]
+tags = data["tags"]
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+bot_name = "Duminda"
+#print("Let's Chat: type 'Quit' to exit")
+
+
+def get_response(msg):
+    sentence = tokenize(msg)
+    x = bag_of_words(sentence, all_words)
+    x = x.reshape(1, x.shape[0])
+    x = torch.from_numpy(x)
+
+    output = model(x)
+    _, predicted = torch.max(output, dim=1)
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+
+    if prob.item() > 0.85:
+        for intent in intents["intents"]:
+            if tag == intent["tag"]:
+                a=(f"{random.choice(intent['responses'])}")
+
+    else:
+        a=(f"sorry i don't understand")
+
+    return a
